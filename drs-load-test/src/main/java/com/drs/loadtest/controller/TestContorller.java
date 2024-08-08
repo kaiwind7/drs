7f4faac034c73f90b66cfa9a420cb63cb860a137
kaiwind7 <kaiwind7@qoo10.com>
2024-08-09 03:22:50 +0900

    [DU-531] [DRS] 성능테스트

--- drs-load-test/src/main/java/com/drs/loadtest/controller/TestContorller.java
+++ drs-load-test/src/main/java/com/drs/loadtest/controller/TestContorller.java
@@ -10,10 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 @RestController
 public class TestContorller {
+    private AtomicInteger requestCount = new AtomicInteger(0);
+
+
     @GetMapping("/health/check")
     public ResponseEntity test() {
         return ResponseEntity.ok("성공");
@@ -21,10 +25,23 @@ public class TestContorller {
 
     @PostMapping("/receive")
     public ResponseEntity receive(@RequestBody Object requestData) {
-        log.info(requestData.toString());
+        requestCount.incrementAndGet();
+
+        log.info("{} :: {}", requestCount.get(), requestData.toString());
         Map<String, String> tempJsonMap = new HashMap<>();
         tempJsonMap.put("message", "receive 성공");
 
         return  ResponseEntity.ok(tempJsonMap);
     }
+
+    @GetMapping("/count")
+    public int getRequestCount() {
+        return requestCount.get();
+    }
+
+    @GetMapping("/init")
+    public void initRequestCount() {
+        requestCount.set(0);
+    }
+
 }
